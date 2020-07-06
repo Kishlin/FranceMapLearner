c8dd79d625e91a9d7ac1f5d8489f519a2279123b
Pierre-Louis Legrand <pllegrand@igraal.com>
2020-07-06 20:32:56 +0200

    Page: Add a home page

---
+++ src/routes/homepage/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import './Home.css';
+
+import LinkButton from "../../components/button/LinkButton";
+
+function Home() {
+    return (
+        <Grid container id="home">
+            <Grid item xs className="home-section">
+                <h1>Regions - Map</h1>
+                <LinkButton url="/learn/regions/location" text="Learn" />
+                <LinkButton url="/quizz/regions/location" text="Quizz" />
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Home;
