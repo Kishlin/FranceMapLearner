551f1d257223c077c0c7497fee17b6e7105649f0
Pierre-Louis Legrand <pllegrand@igraal.com>
2020-07-06 20:28:14 +0200

    Feature: Add the regions data

---
+++ src/data/Regions.js
@@ -0,0 +1,19 @@
+import Region from "../lib/Region";
+
+const REGIONS = [
+    new Region(0, "Grand Est", "Stasbourg", []),
+    new Region(1, "Hauts de France", "Lille", []),
+    new Region(2, "Ile de France", "Paris", []),
+    new Region(3, "Bourgogne - Franche Comté", "Dijon", []),
+    new Region(4, "Centre - Val de Loire", "Orléans", []),
+    new Region(5, "Auvergne - Rhône - Alpes", "Lyon", []),
+    new Region(6, "Nouvelle Acquitaine", "Bordeaux", []),
+    new Region(7, "Normandie", "Rouen", []),
+    new Region(8, "Bretagne", "Stasbourg", []),
+    new Region(9, "Pays de la Loire", "Nantes", []),
+    new Region(10, "Occitanie", "Toulouse", []),
+    new Region(11, "Provence - Alpes - Côte d'Azur", "Marseille", []),
+    new Region(12, "Corse", "Ajaccio", [])
+];
+
+export default REGIONS;
