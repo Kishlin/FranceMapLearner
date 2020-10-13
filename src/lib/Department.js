d435d55c0c3b920d911397e58a78639ff8e0dc43
Pierre-Louis Legrand <pllegrand@igraal.com>
2020-10-13 23:15:56 +0200

    Feature: Add slug to department object

--- src/lib/Department.js
+++ src/lib/Department.js
@@ -1,9 +1,10 @@
 import Entity from "./Entity";
 
 export default class Department extends Entity {
-    constructor(id, name, hometown, region) {
+    constructor(id, name, hometown, slug, region) {
         super(id, name, hometown);
 
         this.region = region;
+        this.slug = slug;
     }
 }
